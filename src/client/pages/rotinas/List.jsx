e73638a0e1f13c4b58f05c870f46e599495012dd
Henrique Fidellis <fidellis@bb.com.br>
2022-01-19 13:15:42 -0300

    alt

--- src/client/pages/rotinas/List.jsx
+++ src/client/pages/rotinas/List.jsx
@@ -2,27 +2,43 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { message } from '~/store/app';
+import { setFilter, setFilters } from '~/store/filter';
 import DataTable from '~/components/data-table/DataTable';
 import Aviso from '~/components/message/Aviso';
 import Button from '~/components/Button';
 import NavigationButton from '~/components/NavigationButton';
+import { Select } from '~/components/form/form/inputs';
+import { Grid } from '@material-ui/core';
 import { getDados } from './hook';
 
+const options = {
+  situacao: [
+    {
+      value: 1,
+      label: 'Vigente'
+    },
+    {
+      value: 2,
+      label: 'Não Vigente',
+    },
+  ]
+};
+
 const columns = {
-  // 'tipo.nome': {
-  //   label: 'Célula',
-  //   width: 100,
-  //   search: true,
-  // },
-  // 'periodicidade.nome': {
-  //   label: 'Periodicidade',
-  //   width: 110,
-  //   search: true,
-  // },
-  // nome: {
-  //   label: 'Nome',
-  //   search: true,
-  // },
+  'tipo.nome': {
+    label: 'Célula',
+    width: 100,
+    search: true,
+  },
+  'periodicidade.nome': {
+    label: 'Periodicidade',
+    width: 110,
+    search: true,
+  },
+  nome: {
+    label: 'Nome',
+    search: true,
+  },
   responsaveis: {
     label: 'Responsáveis',
     search: true,
@@ -32,7 +48,7 @@ const columns = {
 
 const Component = (props) => {
   const [data, setData] = useState([]);
-  const [response] = getDados({ include: ['usuarioInclusao', 'status', 'tipo', 'periodicidade', 'responsaveis'] });
+  const [response] = getDados({ include: ['usuarioInclusao', 'status', 'tipo', 'periodicidade', 'responsaveis'], filter: props.filter.rotina });
 
   useEffect(() => {
     setData(response);
@@ -40,16 +56,29 @@ const Component = (props) => {
 
   return (
     <div>
-      <DataTable
-        rows={data}
-        columns={columns}
-        width="95%"
-        count
-        exportCsv
-        headerHeight={80}
-        onClick={({ row }) => props.history.push(`/rotina/${row.id}`)}
-      />
+      <Grid container spacing={1} alignItems="flex-end">
+        <Grid item xs={2}>
+          <Select
+            id="status_id"
+            label="Situação"
+            options={options.situacao}
+            value={props.filter.rotina.status_id}
+            onChange={({ id, value }) => props.setFilter({ id, value, filter: 'rotina' })} />
+        </Grid>
+        <Grid item xs={12}>
+          <DataTable
+            rows={data}
+            columns={columns}
+            count
+            exportCsv
+            headerHeight={80}
+            onClick={({ row }) => props.history.push(`/rotina/${row.id}`)}
+          />
+        </Grid>
 
+
+
+      </Grid>
       <NavigationButton buttons={[
         {
           label: 'Adicionar Rotina',
@@ -62,7 +91,7 @@ const Component = (props) => {
 };
 
 Component.propTypes = {};
-const mapStateToProps = ({ app: { usuario } }) => ({ usuario });
-const mapDispatchToProps = ({ message });
+const mapStateToProps = ({ app: { usuario }, filter }) => ({ usuario, filter });
+const mapDispatchToProps = ({ message, setFilter, setFilters });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Component);
--- src/client/pages/rotinas/hook.js
+++ src/client/pages/rotinas/hook.js
@@ -9,7 +9,7 @@ export function getDados(params) {
   async function change() {
     setLoading(true);
     const [response] = await Promise.all([
-      getData('/rotina/rotina', { ...params, order: ['id'] }),
+      getData('/rotina/rotina', { ...params, ...params.filter, order: ['id'] }),
     ]);
     setData(response);
     setLoading(false);
