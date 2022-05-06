e7a7b720a7442702d75d937ba2d08a9762d3952e
Henrique Fidellis <fidellis@bb.com.br>
2022-05-06 16:53:28 +0100

    v0.0.61

---
+++ server/models/demanda/demanda.js
@@ -0,0 +1,97 @@
+// import sequelize from 'common/sequelize';
+// import Sequelize from 'common/sequelize/sequelize';
+// import Version from 'common/sequelize/version';
+// import Usuario from 'common/models/portal/usuario';
+// // import Tipo from './tipo';
+// // import Status from './status';
+
+// const Model = sequelize.define(
+//     'demanda',
+//     {
+//         id: {
+//             type: Sequelize.BIGINT,
+//             primaryKey: true,
+//             autoIncrement: true,
+//         },
+
+//         titulo: {
+//             type: Sequelize.STRING(255),
+//             allowNull: false,
+//             // unique: true,
+//         },
+
+//         tipo_id: {
+//             type: Sequelize.INTEGER,
+//             allowNull: true,
+//         },
+
+//         status_id: {
+//             type: Sequelize.INTEGER,
+//             allowNull: false,
+//             defaultValue: 1,
+//         },
+
+//         uorInclusao_id: {
+//             type: Sequelize.INTEGER,
+//             allowNull: false,
+//         },
+
+//         usuarioInclusao_id: {
+//             type: Sequelize.STRING(9),
+//             allowNull: false,
+//             associate: {
+//                 model: 'Usuario',
+//             },
+//         },
+
+//         dataHoraInclusao: {
+//             type: Sequelize.DATE,
+//             allowNull: false,
+//             defaultValue: Sequelize.NOW,
+//         },
+
+//         uorResponsavel_id: {
+//             type: Sequelize.INTEGER,
+//             allowNull: false,
+//         },
+
+//         usuarioAlteracao_id: {
+//             type: Sequelize.STRING(9),
+//             allowNull: true,
+//             associate: {
+//                 model: 'Usuario',
+//             },
+//         },
+
+//         dataHoraAlteracao: {
+//             type: Sequelize.DATE,
+//             allowNull: true,
+//         },
+
+
+//     },
+//     {
+//         // defaultScope: {
+//         //     include: [
+//         //         // {
+//         //         //     model: Tipo,
+//         //         //     as: 'tipo',
+//         //         // },
+//         //         {
+//         //             model: Status,
+//         //             as: 'status',
+//         //         }
+//         //     ]
+//         // },
+//         schema: 'demanda',
+//         tableName: 'demanda',
+//     },
+// );
+
+// // Model.belongsTo(Tipo, { as: 'tipo', foreignKey: 'tipo_id' });
+// // Model.belongsTo(Status, { as: 'status', foreignKey: 'status_id' });
+
+// // const ModelVersion = new Version(Model);
+// // ModelVersion.sync();
+// export default Model;
+"use strict";
---
+++ server/models/demanda/status.js
@@ -0,0 +1,47 @@
+// import sequelize from 'common/sequelize';
+// import Sequelize from 'common/sequelize/sequelize';
+
+// const Model = sequelize.define(
+//     'statusDemanda',
+//     {
+//         id: {
+//             type: Sequelize.BIGINT,
+//             primaryKey: true,
+//             autoIncrement: true,
+//         },
+
+//         nome: {
+//             type: Sequelize.STRING(255),
+//             allowNull: false,
+//         },
+
+//         ativo: {
+//             type: Sequelize.BOOLEAN,
+//             allowNull: false,
+//             defaultValue: true,
+//         },
+
+//         statusMovimentaca_id: {
+//             type: Sequelize.ARRAY(Sequelize.INTEGER),
+//             allowNull: true,
+//         },
+//     },
+//     {
+//         schema: 'demanda',
+//         tableName: 'status',
+//     },
+// );
+
+// // Model.afterSync(() => Promise.all([
+// //     Model.upsert({ id: 1, nome: 'Aguardando atendimento', statusMovimentaca_id: [1, 4] }),
+// //     Model.upsert({ id: 2, nome: 'Em análise', statusMovimentaca_id: [1, 4] }),
+// //     Model.upsert({ id: 3, nome: 'Devolvido', statusMovimentaca_id: [3] }),
+// //     Model.upsert({ id: 4, nome: 'Em execução', statusMovimentaca_id: [1, 4] }),
+// //     Model.upsert({ id: 5, nome: 'Em homologação', statusMovimentaca_id: [2] }),
+// //     Model.upsert({ id: 6, nome: 'Homologado', statusMovimentaca_id: [2] }),
+// //     Model.upsert({ id: 7, nome: 'Finalizado' }),
+// //     Model.upsert({ id: 8, nome: 'Cancelado' }),
+// // ]));
+
+// export default Model;
+"use strict";
